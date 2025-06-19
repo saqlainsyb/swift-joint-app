e500946db2b546461af13f374b971ebed0aa0264
saqlainsyb <saqlainsyb@gmail.com>
2025-06-19 11:03:51 +0530

    Changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import RootLayout from '@/components/layout/RootLayout'
 import Home from '@/pages/Home'
 import ApplicationPage from '@/pages/ApplicationPage'
 import ProductDetailPage from '@/pages/ProductDetailPage'
+import { useEffect } from 'react'
 
 function App() {
+  const location = useLocation();
+
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]); 
   return (
     <RootLayout>
         <Routes>
